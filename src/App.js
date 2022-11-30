c55bb7384b73d37553a149b3b90953b86fc56e8b
tikameutia <111536774+tikameutia@users.noreply.github.com>
2022-11-30 16:17:29 +0100

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello from App.js</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>React Weather App</h1>
+        <button className="btn btn-primary">Click</button>
+      </div>
+    </div>
+  );
 }
