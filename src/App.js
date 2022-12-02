32579b8a53413441cf007215cd9fe12ac1f895af
tikameutia <111536774+tikameutia@users.noreply.github.com>
2022-12-02 10:52:23 +0100

    Added coded-by footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://hilarious-pie-d37c2c.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tika Lubis
+        </a>{" "}
+        and open-sourced on{" "}
+        <a
+          href="https://github.com/tikameutia/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
