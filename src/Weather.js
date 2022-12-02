9a160d5c37ca3de3e3f8854804b11df92b6b7e9d
tikameutia <111536774+tikameutia@users.noreply.github.com>
2022-12-02 16:12:22 +0100

    Created weather temperature and description

--- src/Weather.js
+++ src/Weather.js
@@ -5,8 +5,22 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
+    <div className="Weather container">
       <SearchEngine />
+      <h1>Jakarta</h1>
+      <div className="d-flex justify-content-around align-item-center">
+        <div className="col-auto">
+          <img
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt="light rain"
+            width="150"
+          />
+        </div>
+        <div className="col-auto">
+          <div className="temp">24º</div>
+          <div className="temp-desc">Light Rain</div>
+        </div>
+      </div>
     </div>
   );
 }
